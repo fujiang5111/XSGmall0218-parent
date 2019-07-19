1d992b6bc1e18437a7a136fdc60d7cf098746694
g51099 <g51099@126.com>
2019-07-19 21:05:06 +0800

    Initial commit

---
+++ XSGmall0218-common/src/main/java/com/gmall/test/Hello.java
@@ -0,0 +1,12 @@
+package com.gmall.test;
+
+/**
+ * @author xiao-shan
+ * @create 2019-07-19 21:02
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+}
